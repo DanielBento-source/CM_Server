9f77f0ff3dd1b57da761047c81ead4b1a2ce8bc6
DanielBento <dsbld2@gmail.com>
2022-12-01 22:37:03 -0300

    feat(prisma): module & service criados

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MusicoModule } from './musico/musico.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [MusicoModule],
+  imports: [MusicoModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
