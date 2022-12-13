e079a9458671019c6589631caa9511f7ff7616f8
DanielBento <dsbld2@gmail.com>
2022-12-13 20:58:39 -0300

    feat(user): implent dto & entity

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,56 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, Matches, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do usuário - Apenas para exibição',
+    example: 'Daniel',
+  })
+  nome: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Sobrenome do usuário - Apenas para exibição',
+    example: 'Bento',
+  })
+  sobrenome: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'nickname do usuário, deve ser único -  Utilizado no login',
+    example: 'AdminSaoBentoCampinas',
+  })
+  nickname: string;
+
+  @IsString()
+  @MinLength(6)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Senha muito fraca',
+  })
+  @ApiProperty({
+    description: 'Senha do usuário para login',
+    example: 'Abcd@1234',
+  })
+  password: string;
+
+  @ApiProperty({
+    description: 'A confirmação deve ser igual a senha',
+    example: 'Abcd@1234',
+  })
+  confirmPassword: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Numero de telefone do usuário com ddd',
+    example: '19991619186',
+  })
+  fone: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Email do usuário',
+    example: 'dsbld2@gmail.com',
+  })
+  email: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class User {}
+export class User {
+  id?: string;
+  cadUserGeral?: number;
+  nome: string;
+  sobrenome: string;
+  nickname: string;
+  password: string;
+  fone: string;
+  email: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
