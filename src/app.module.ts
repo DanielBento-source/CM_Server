1b35c7ea396e5b9b208729a290774372061c0efa
DanielBento <dsbld2@gmail.com>
2022-12-13 20:12:19 -0300

    feat(user): generate resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MusicoModule } from './musico/musico.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { InstrumentoModule } from './instrumento/instrumento.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MusicoModule, PrismaModule, InstrumentoModule],
+  imports: [MusicoModule, PrismaModule, InstrumentoModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
