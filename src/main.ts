e6919721f554b2a866b78bcc3c546d88cb9261ce
DanielBento <dsbld2@gmail.com>
2022-12-14 18:32:32 -0300

    feat(user): implement "controller" & "Service" & add tags

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     .setDescription('Aplicação para gestão dos muscicos da ccb')
     .setVersion('1.0.0')
     .addTag('status')
+    .addTag('user')
     .addTag('musico')
     .addTag('instrumento')
     .build();
