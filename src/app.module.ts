67c4551bb55eb350a80677e7c30093891109c579
DanielBento <dsbld2@gmail.com>
2023-01-24 23:18:02 -0300

    feat: create "naipe" resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NaipeModule } from './naipe/naipe.module';
 
 @Module({
-  imports: [],
+  imports: [NaipeModule],
   controllers: [AppController],
   providers: [AppService],
 })
