4e0fa179160d9247d94aed01c4c2e40cda38c793
DanielBento <dsbld2@gmail.com>
2023-01-27 20:13:45 -0300

    feat(prisma): module & service created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NaipeModule } from './naipe/naipe.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [NaipeModule],
+  imports: [NaipeModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
