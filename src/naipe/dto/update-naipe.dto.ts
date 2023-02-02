c03adc059bf89b7b00e3f67a2a7bc7297dce751d
DanielBento <dsbld2@gmail.com>
2023-02-02 00:04:58 -0300

    fix(naipe): change 'PartialType' to swagger  on 'UpdateNaipeDto'

--- src/naipe/dto/update-naipe.dto.ts
+++ src/naipe/dto/update-naipe.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateNaipeDto } from './create-naipe.dto';
 
 export class UpdateNaipeDto extends PartialType(CreateNaipeDto) {}
