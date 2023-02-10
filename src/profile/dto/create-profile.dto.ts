d1a6b878c6ece8baf252d408df49d3b1410ec828
DanielBento <dsbld2@gmail.com>
2023-02-10 20:02:01 -0300

    feat(profile): implement 'create' & 'update' dto.

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,1 +1,43 @@
-export class CreateProfileDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUrl, Matches, MinLength } from 'class-validator';
+
+export class CreateProfileDto {
+  @IsString()
+  @ApiProperty({
+    description: 'nickname do usuário, Utilzado no login, deve ser único.',
+    example: 'SAOBENTOCAMPINASSP',
+  })
+  nickname: string;
+
+  @IsString()
+  @MinLength(6)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Senha muito fraca.',
+  })
+  @ApiProperty({
+    description: 'Senha do usuário para login.',
+    example: 'Abcd@1234',
+  })
+  password: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'A confirmação da senha deve ser igual a senha.',
+    example: 'Abcd@1234',
+  })
+  confirmPassword: string;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem de perfil do usuário.',
+    example: 'https://avatars.githubusercontent.com/u/62419185?v=4',
+  })
+  image: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Ocupação do dono do perfil.',
+    example: 'Instrutor',
+  })
+  occupation: string;
+}
--- src/profile/entities/profile.entity.ts
+++ src/profile/entities/profile.entity.ts
@@ -1,1 +1,9 @@
-export class Profile {}
+export class Profile {
+  id?: string;
+  nickname: string;
+  password: string;
+  imageProfile: string;
+  occupation?: string | null;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
