57361c22b422de1aa49e58cb5eff4965e54f4c7f
DanielBento <dsbld2@gmail.com>
2023-02-10 20:20:28 -0300

    feat(profile): implement 'Service'

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,26 +1,113 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { Profile } from './entities/profile.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class ProfileService {
-  create(createProfileDto: CreateProfileDto) {
-    return 'This action adds a new profile';
+  private profileSelect = {
+    id: true,
+    nickname: true,
+    password: false,
+    imageProfile: true,
+    occupation: true,
+    createdAt: true,
+    updatedAt: false,
+  };
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(dto: CreateProfileDto): Promise<Profile> {
+    if (dto.password != dto.confirmPassword) {
+      throw new BadRequestException('As senhas informadas não são iguais!');
+    }
+    delete dto.confirmPassword;
+
+    const data: Profile = {
+      ...dto,
+      password: await bcrypt.hash(dto.password, 10),
+    };
+
+    return this.prisma.profile
+      .create({
+        data,
+        select: this.profileSelect,
+      })
+      .catch(this.handleError);
   }
 
-  findAll() {
-    return `This action returns all profile`;
+  findAll(): Promise<Profile[]> {
+    return this.prisma.profile.findMany({
+      select: this.profileSelect,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} profile`;
+  async findById(id: string): Promise<Profile> {
+    const record = await this.prisma.profile.findUnique({
+      where: { id },
+      select: this.profileSelect,
+    });
+
+    if (!record) {
+      throw new NotFoundException(`Registro com o Id '${id}' não encontrado.`);
+    }
+    return record;
+  }
+  async findOne(id: string): Promise<Profile> {
+    return this.findById(id);
   }
 
-  update(id: number, updateProfileDto: UpdateProfileDto) {
-    return `This action updates a #${id} profile`;
+  async update(id: string, dto: UpdateProfileDto): Promise<Profile> {
+    await this.findById(id);
+
+    if (dto.password) {
+      if (dto.password != dto.confirmPassword) {
+        throw new BadRequestException('As senhas informadas não são iguais!');
+      }
+    }
+
+    delete dto.confirmPassword;
+
+    const data: Partial<Profile> = {
+      ...dto,
+    };
+
+    if (data.password) {
+      data.password = await bcrypt.hash(data.password, 10);
+    }
+
+    return this.prisma.profile
+      .update({
+        where: { id },
+        data,
+        select: this.profileSelect,
+      })
+      .catch(this.handleError);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} profile`;
+  async remove(id: string) {
+    await this.findById(id);
+
+    await this.prisma.profile.delete({
+      where: { id },
+    });
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
+
+    if (!lastErrorLine) {
+      console.log(error);
+    }
+    throw new UnprocessableEntityException(
+      lastErrorLine || 'Algum erro ocorreu ao executar a operação',
+    );
   }
 }
